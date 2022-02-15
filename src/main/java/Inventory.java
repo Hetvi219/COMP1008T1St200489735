706a038121459a150b4109cbb941bdeb217d1786
Hetvi219 <200489735@student.georgianc.on.ca>
2022-02-15 15:15:18 +0530

    Question 2.1 complete Question 2.2 complete Question 2.3 complete Question 2.4 complete Question 2.5 complete Question 2.6 complete Question 3.1 complete

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -1,2 +1,42 @@
-public class Inventory {
+import java.util.ArrayList;
+
+public class Inventory{
+    private ArrayList<Phone> phones;
+
+    public Inventory() {
+        phones = new ArrayList<>();
+    }
+    public void addPhone(Phone phone){
+        phones.add(phone);
+    }
+    public int getNumOfPhones(){
+        return phones.size();
+    }
+    public double getInventoryValue(){
+        double inventoryValue = 0;
+        for(Phone phone: phones){
+            inventoryValue += phone.getPrice();
+        }
+        return inventoryValue;
+    }
+    public double getAvgPrice(){
+        if(phones.size() == 0) return 0;
+
+        double price = 0;
+        for(Phone phone: phones){
+            price += phone.getPrice();
+        }
+        return price / phones.size();
+    }
+    //bonus method
+    public double getCheapestPhone(){
+        if(phones.size() == 0) return 0;
+
+        double price = phones.get(0).getPrice();
+        for(Phone phone: phones){
+            if(phone.getPrice() < price) price = phone.getPrice();
+        }
+        return price;
+    }
 }
+
