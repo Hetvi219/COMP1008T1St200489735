cd0e4b07d32ebe342dc2df165dc9d9d041cefea2
Hetvi219 <200489735@student.georgianc.on.ca>
2022-02-15 15:06:21 +0530

    Question 2.1 complete

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Phone {
+    private String make;
+    private String model;
+    private int memory;
+    private Double price;
+    //create this arraylist of strings, we will use it to validate the manufacturer
+    ArrayList<String> validMakes = new ArrayList<>();
+
+    public Phone(String make, String model, int memory, Double price) {
+        this.make = make;
+        this.model = model;
+        this.memory = memory;
+        this.price = price;
+    }
 }
